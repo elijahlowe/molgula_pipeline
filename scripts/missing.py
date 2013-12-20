6e61c27fe522cb3e3506fea24cdb506646b5b1a4
C. Titus Brown <titus@idyll.org>
2013-12-20 16:05:46 +0000

    added missing.py for create file with missing blast hits

---
+++ scripts/missing.py
@@ -0,0 +1,21 @@
+import screed
+import blastparser
+import sys
+
+infile = sys.argv[1]
+blast_file = sys.argv[2]
+
+lengths={}
+
+print blast_file
+
+recov=[]
+cnt=0
+for hits in blastparser.parse_file(blast_file):
+    recov.append(hits.query_name)
+    cnt=cnt+1
+
+for n, record in enumerate(screed.open(infile)):
+    if record['name'] not in recov:
+        '>%s/2\n%s' % (record[name], record['sequence'])
+
